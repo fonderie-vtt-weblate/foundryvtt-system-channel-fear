7062acaf346f0fe0bff947161e715dcd5e454a4b
DjLeChuck <djlechuck@gmail.com>
2022-07-25 15:31:52 +0200

    feat: initial commit🚀

---
+++ module/helpers/config.mjs
@@ -0,0 +1,31 @@
+export const CHANNELFEAR = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ CHANNELFEAR.abilities = {
+  "act": "CHANNELFEAR.AbilityAct",
+  "kno": "CHANNELFEAR.AbilityKno",
+  "bea": "CHANNELFEAR.AbilityBea",
+  "for": "CHANNELFEAR.AbilityFor",
+  "per": "CHANNELFEAR.AbilityPer",
+  "imp": "CHANNELFEAR.AbilityImp",
+  "knh": "CHANNELFEAR.AbilityKnh",
+  "fig": "CHANNELFEAR.AbilityFig",
+  "sel": "CHANNELFEAR.AbilitySel",
+  "aim": "CHANNELFEAR.AbilityAim"
+};
+
+CHANNELFEAR.abilityAbbreviations = {
+  "act": "CHANNELFEAR.AbilityActAbbr",
+  "kno": "CHANNELFEAR.AbilityKnoAbbr",
+  "bea": "CHANNELFEAR.AbilityBeaAbbr",
+  "for": "CHANNELFEAR.AbilityForAbbr",
+  "per": "CHANNELFEAR.AbilityPerAbbr",
+  "imp": "CHANNELFEAR.AbilityImpAbbr",
+  "knh": "CHANNELFEAR.AbilityKnhAbbr",
+  "fig": "CHANNELFEAR.AbilityFigAbbr",
+  "sel": "CHANNELFEAR.AbilitySelAbbr",
+  "aim": "CHANNELFEAR.AbilityAimAbbr"
+};
