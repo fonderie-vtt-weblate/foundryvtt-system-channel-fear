6473dbf353e898498fd46ae6d5b53f7e00461278
Vivien DE BONA <vdebona@umanit.fr>
2022-08-21 22:16:03 +0200

    style: remove useless code and code style

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -1,51 +1,15 @@
-/**
- * Extend the base Actor document by defining a custom roll data structure which is ideal for the Simple system.
- * @extends {Actor}
- */
 export class ChannelFearActor extends Actor {
-
-  /** @override */
-  prepareData() {
-    // Prepare data for the actor. Calling the super version of this executes
-    // the following, in order: data reset (to clear active effects),
-    // prepareBaseData(), prepareEmbeddedDocuments() (including active effects),
-    // prepareDerivedData().
-    super.prepareData();
-  }
-
-  /** @override */
-  prepareBaseData() {
-    // Data modifications in this step occur before processing embedded
-    // documents or derived data.
-  }
-
-  /**
-   * @override
-   * Augment the basic actor data with additional dynamic data. Typically,
-   * you'll want to handle most of your calculated/derived data in this step.
-   * Data calculated in this step should generally not exist in template.json
-   * (such as ability modifiers rather than ability scores) and should be
-   * available both inside and outside of character sheets (such as if an actor
-   * is queried and has a roll executed directly from it).
-   */
   prepareDerivedData() {
     const actorData = this.data;
-    const data = actorData.data;
-    const flags = actorData.flags.channelfear || {};
 
-    // Make separate methods for each Actor type (character, npc, etc.) to keep
-    // things organized.
     this._prepareCharacterData(actorData);
-    this._prepareNpcData(actorData);
   }
 
-  /**
-   * Prepare Character type specific data
-   */
   _prepareCharacterData(actorData) {
-    if ('character' !== actorData.type) return;
+    if ('character' !== actorData.type) {
+      return;
+    }
 
-    // Make modifications to data here. For example:
     const data = actorData.data;
 
     // Ensure health and resource are between allowed boundaries
@@ -70,29 +34,14 @@ export class ChannelFearActor extends Actor {
     return value;
   }
 
-  /**
-   * Prepare NPC type specific data.
-   */
-  _prepareNpcData(actorData) {
-    if ('npc' !== actorData.type) return;
-  }
-
-  /**
-   * Override getRollData() that's supplied to rolls.
-   */
   getRollData() {
     const data = super.getRollData();
 
-    // Prepare character roll data.
     this._getCharacterRollData(data);
-    this._getNpcRollData(data);
 
     return data;
   }
 
-  /**
-   * Prepare character roll data.
-   */
   _getCharacterRollData(data) {
     if (this.data.type !== 'character') return;
 
@@ -107,14 +56,5 @@ export class ChannelFearActor extends Actor {
         data[k] = v;
       }
     }
-  }
-
-  /**
-   * Prepare NPC roll data.
-   */
-  _getNpcRollData(data) {
-    if (this.data.type !== 'npc') return;
-
-    // Process additional NPC data here.
   }
 }
--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -1,32 +1,21 @@
 import * as Dice from '../dice.mjs';
 
-/**
- * Extend the basic Item with some very simple modifications.
- * @extends {Item}
- */
 export class ChannelFearItem extends Item {
-  /**
-   * Augment the basic Item data model with additional dynamic data.
-   */
   prepareData() {
-    // As with the actor class, items are documents that can have their data
-    // preparation methods overridden (such as prepareBaseData()).
     super.prepareData();
   }
 
   prepareDerivedData() {
     const itemData = this.data;
 
-    this._prepareSpecialtiesData(itemData);
-  }
-
-  _prepareSpecialtiesData(itemData) {
     this._prepareSpecialtyData(itemData);
     this._prepareWeaponData(itemData);
   }
 
   _prepareSpecialtyData(itemData) {
-    if ('specialty' !== itemData.type) return;
+    if ('specialty' !== itemData.type) {
+      return;
+    }
 
     const data = itemData.data;
 
@@ -39,7 +28,9 @@ export class ChannelFearItem extends Item {
   }
 
   _prepareWeaponData(itemData) {
-    if ('weapon' !== itemData.type) return;
+    if ('weapon' !== itemData.type) {
+      return;
+    }
 
     const data = itemData.data;
 
@@ -56,13 +47,10 @@ export class ChannelFearItem extends Item {
     }
   }
 
-  /**
-   * Prepare a data object which is passed to any Roll formulas which are created related to this Item
-   * @private
-   */
   getRollData() {
-    // If present, return the actor's roll data.
-    if (!this.actor) return null;
+    if (!this.actor) {
+      return null;
+    }
 
     const rollData = this.actor.getRollData();
     rollData.item = foundry.utils.deepClone(this.data.data);
@@ -70,58 +58,15 @@ export class ChannelFearItem extends Item {
     return rollData;
   }
 
-  /**
-   * Handle clickable rolls.
-   * @private
-   */
   async roll() {
     const item = this.data;
 
     if ('specialty' === item.type) {
-      return this._rollSpecialty(item);
+      return await Dice.specialtyCheck(item);
     }
 
     if ('weapon' === item.type) {
-      return this._rollWeapon(item);
+      return await Dice.weaponCheck(item);
     }
-
-    // Initialize chat data.
-    const speaker = ChatMessage.getSpeaker({ actor: this.actor });
-    const rollMode = game.settings.get('core', 'rollMode');
-    const flavor = `[${item.type}] ${item.name}`;
-
-    // If there's no roll data, send a chat message.
-    if (!this.data.data.formula) {
-      ChatMessage.create({
-        speaker: speaker,
-        rollMode: rollMode,
-        flavor,
-        content: item.data.description ?? '',
-      });
-    }
-    // Otherwise, create a roll and send a chat message from it.
-    else {
-      // Retrieve roll data.
-      const rollData = this.getRollData();
-
-      // Invoke the roll and submit it to chat.
-      const roll = new Roll(rollData.item.formula, rollData);
-      // If you need to store the value first, uncomment the next line.
-      // let result = await roll.roll({async: true});
-      roll.toMessage({
-        speaker,
-        rollMode,
-        flavor,
-      });
-      return roll;
-    }
-  }
-
-  async _rollSpecialty(specialty) {
-    await Dice.specialtyCheck(specialty);
-  }
-
-  async _rollWeapon(weapon) {
-    await Dice.weaponCheck(weapon);
   }
 }
